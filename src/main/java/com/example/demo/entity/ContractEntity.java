c4e472d74c73fa5586d25d7c31e37ac2cb77c957
bala-amypo <bala@amypo.in>
2025-12-18 17:22:06 +0000

    User commit

--- src/main/java/com/example/demo/entity/ContractEntity.java
+++ src/main/java/com/example/demo/entity/ContractEntity.java
@@ -1,8 +1,10 @@
 package com.example.demo.entity;
 
 import java.util.Date;
-
+@Entity
 public class ContractEntity{
+    @Id
+    @GeneratedValue(strategy=GenerationType.IDENTITY)
     private Long id;
     private String contractNumber;
     private String title;
@@ -72,54 +74,16 @@ public class ContractEntity{
     String counterpartyName,Date agreedDeliveryDate,
     BigDecimal baseContractValue,String status,LocalDateTime createdAt,
     LocalDateTime updatedAt){
-    public void setId(Long id){
         this.id=id;
-    }
-    public Long getId(){
-        return id;
-    }
-    public void setContractNumber(String contractNumber){
         this.contractNumber=contractNumber;
-    }
-    public String getContractNumber(){
-        return contractNumber;
-    }
-    public void setTitle(String title){
-        this.title=title;
-    }
-    public String getTitle(){
-        return title;
-    }
-    public void setCounterpartyName(String counterpartyName){
         this.counterpartyName=counterpartyName;
-    }
-    public  String getCounterpartyName(){
-        return counterpartyName;
-    }
-    public void SetAgreedDeliveryDate(String agreedDeliveryDate){
         this.agreedDeliveryDate=agreedDeliveryDate;
-    }
-    public  Date getAgreedDeliveryDate(){
-        return agreedDeliveryDate;
-    }
-    public void setBaseContract(BigDecimal title){
         this.basecontract=basecontract;
-    }
-    public BigDecimal getbasecontract(){
-        return basecontract;
-    }
-    public void setStatus(String status){
         this.status=status;
-    }
-    public String getStatus(){
-        return status;
-    }
-    public void setcreatedAt(LocalDateTime createdAt){
         this.createdAt=createdAt;
-    }
-    public String getCreatedAt(){
-        return createdAt;
-    }
-    public void setUpdatedAt(LocalDateTime updatedAt){
         this.updatedAt=updatedAt;
+    }
+    public ContractEntity(){
+
+    }
 }
