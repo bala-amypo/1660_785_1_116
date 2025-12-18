855cd57f366f040b5a0190e9005f3d51a146e66f
bala-amypo <bala@amypo.in>
2025-12-18 17:37:59 +0000

    User commit

--- src/main/java/com/example/demo/entity/DeliveryRecordEntity.java
+++ src/main/java/com/example/demo/entity/DeliveryRecordEntity.java
@@ -11,4 +11,29 @@ public class DeliveryRecordEntity{
     private Date deliveryDate;
     private String notes;
     private LocalDateTime createdAt;
+
+    public void setId(Long id){
+        this.id=id;
+    }
+    public Long getId(){
+        return id;
+    }
+    public void setContract(Contract contract){
+        this.contract=contract;
+    }
+    public String getContract(){
+        return contractNumber;
+    }
+    public void SetDeliveryDate(Date DeliveryDate){
+        this.agreDeliveryDate=agreedDeliveryDate;
+    }
+    public  Date getAgreedDeliveryDate(){
+        return agreedDeliveryDate;
+    }
+    public void setNotes(String notes){
+        this.notes=notes;
+    }
+    public String getNotes(){
+        return notes;
+    }
 }
