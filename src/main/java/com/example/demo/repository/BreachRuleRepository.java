85c4ef2f1db588856ef2734abe53c8eb15fbeae8
bala-amypo <bala@amypo.in>
2025-12-18 18:13:59 +0000

    User commit

--- src/main/java/com/example/demo/repository/BreachRuleRepository.java
+++ src/main/java/com/example/demo/repository/BreachRuleRepository.java
@@ -2,7 +2,9 @@ package com.example.demo.repository;
 
 import org.springframework.stereotype.Repository;
 import org.springframework.data.jpa.repository.JpaRepository;
+import com.example.demo.entity.BreachRuleEntity;
 
+@Repository
 public interface BreachRuleRepository extends JpaRepository<BreachRuleRepository,Long>{
     
 }
--- src/main/java/com/example/demo/repository/ContractRepository.java
+++ src/main/java/com/example/demo/repository/ContractRepository.java
@@ -2,4 +2,12 @@ package com.example.demo.repository;
 
 public interface ContractRepository{
     
+import org.springframework.stereotype.Repository;
+import org.springframework.data.jpa.repository.JpaRepository;
+import com.example.demo.entity.ContractEntity;
+
+@Repository
+public interface BreachRuleRepository extends JpaRepository<BreachRuleRepository,Long>{
+    
+}
 }
