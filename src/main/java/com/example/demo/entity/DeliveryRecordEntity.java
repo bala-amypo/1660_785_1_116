29b0ea544e6e9af5149f20b1deb9b0254e6c6c3c
bala-amypo <bala@amypo.in>
2025-12-18 17:31:06 +0000

    User commit

--- src/main/java/com/example/demo/entity/DeliveryRecordEntity.java
+++ src/main/java/com/example/demo/entity/DeliveryRecordEntity.java
@@ -1,5 +1,14 @@
 package com.example.demo.entity;
 
+import java.util.Date;
+import jakarta.persistence.Id;
+@Entity
 public class DeliveryRecordEntity{
-    
+     @Id
+    @GeneratedValue(strategy=GenerationType.IDENTITY)
+    private Long id;
+    private Contract contract;
+    private Date deliveryDate;
+    private String notes;
+    private LocalDateTime createdAt;
 }
