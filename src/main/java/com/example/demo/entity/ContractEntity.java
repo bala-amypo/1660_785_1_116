02e059d91b19bb2522cffec3e69356b2c3a52bea
bala-amypo <bala@amypo.in>
2025-12-18 17:19:06 +0000

    User commit

--- src/main/java/com/example/demo/entity/ContractEntity.java
+++ src/main/java/com/example/demo/entity/ContractEntity.java
@@ -68,5 +68,58 @@ public class ContractEntity{
         return updatedAt;
     }
 
-
+    public ContractEntity(Long id,String contractNumber,String title,
+    String counterpartyName,Date agreedDeliveryDate,
+    BigDecimal baseContractValue,String status,LocalDateTime createdAt,
+    LocalDateTime updatedAt){
+    public void setId(Long id){
+        this.id=id;
+    }
+    public Long getId(){
+        return id;
+    }
+    public void setContractNumber(String contractNumber){
+        this.contractNumber=contractNumber;
+    }
+    public String getContractNumber(){
+        return contractNumber;
+    }
+    public void setTitle(String title){
+        this.title=title;
+    }
+    public String getTitle(){
+        return title;
+    }
+    public void setCounterpartyName(String counterpartyName){
+        this.counterpartyName=counterpartyName;
+    }
+    public  String getCounterpartyName(){
+        return counterpartyName;
+    }
+    public void SetAgreedDeliveryDate(String agreedDeliveryDate){
+        this.agreedDeliveryDate=agreedDeliveryDate;
+    }
+    public  Date getAgreedDeliveryDate(){
+        return agreedDeliveryDate;
+    }
+    public void setBaseContract(BigDecimal title){
+        this.basecontract=basecontract;
+    }
+    public BigDecimal getbasecontract(){
+        return basecontract;
+    }
+    public void setStatus(String status){
+        this.status=status;
+    }
+    public String getStatus(){
+        return status;
+    }
+    public void setcreatedAt(LocalDateTime createdAt){
+        this.createdAt=createdAt;
+    }
+    public String getCreatedAt(){
+        return createdAt;
+    }
+    public void setUpdatedAt(LocalDateTime updatedAt){
+        this.updatedAt=updatedAt;
 }
