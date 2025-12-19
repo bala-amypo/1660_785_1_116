980f66ca956298f1ed585e4aa88d3dab246914ad
bala-amypo <bala@amypo.in>
2025-12-19 16:56:14 +0000

    User commit

--- src/main/java/com/example/demo/entity/PenaltyCalculationEntity.java
+++ src/main/java/com/example/demo/entity/PenaltyCalculationEntity.java
@@ -6,13 +6,9 @@ import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import java.time.LocalDateTime;
 import java.math.BigDecimal;
-import lombok.Data;
-import lombok.AllArgsConstructor;
-import lombok.NoArgsConstructor;
+
 @Entity
-@Data
-@AllArgsConstructor
-@NoArgsConstructor
+
 public class PenaltyCalculationEntity{
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -20,4 +16,48 @@ public class PenaltyCalculationEntity{
     private Integer daysDelayed;
     private BigDecimal calculatedPenalty;
     private LocalDateTime calculatedAt;
+
+    public void setId(Long id){
+        this.id=id;
+    }
+    public Long getId(){
+        return id;
+    }
+    public void setContract(Contract contract){
+        this.contract=contract;
+    }
+    public Contract getContract(){
+        return contract;
+    }
+    public void setDeliveryDate(Date deliveryDate){
+        this.deliveryDate=deliveryDate;
+    }
+    public  Date getDeliveryDate(){
+        return deliveryDate;
+    }
+    public void setNotes(String notes){
+        this.notes=notes;
+    }
+    public String getNotes(){
+        return notes;
+    }
+    public void setCreatedAt(LocalDateTime createdAt){
+        this.createdAt=createdAt;
+    }
+    public LocalDateTime getCreatedAt(){
+        return createdAt;
+    }
+
+    public DeliveryRecordEntity(Long id, Contract contract, Date deliveryDate,
+        String notes, LocalDateTime createdAt){
+
+        this.id=id;
+        this.contract=contract;
+        this.deliveryDate=deliveryDate;
+        this.notes=notes;
+        this.createdAt=createdAt;
+    }
+    public  DeliveryRecordEntity(){
+    }
+}
 }
