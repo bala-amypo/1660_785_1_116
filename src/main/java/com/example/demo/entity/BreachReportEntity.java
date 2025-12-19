6a7d2a1f84853cca29a43243c6db12e29e365c22
bala-amypo <bala@amypo.in>
2025-12-19 17:14:14 +0000

    User commit

--- src/main/java/com/example/demo/entity/BreachReportEntity.java
+++ src/main/java/com/example/demo/entity/BreachReportEntity.java
@@ -1,5 +1,20 @@
 package com.example.demo.entity;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import java.time.LocalDateTime;
+import java.math.BigDecimal;
+
+@Entity
 public class BreachRepositoryEntity{
-    
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @OneToOne
+    private Contract contract;
+    private LocalTimeDate reportGeneratedAt;
+    private Integer daysDelayed;
+    private BigDecimal Penalty;
 }
