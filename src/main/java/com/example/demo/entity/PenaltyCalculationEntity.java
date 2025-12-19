0a5d12cfd012e62a767b8cc9c554c74c2b7bf04f
bala-amypo <bala@amypo.in>
2025-12-19 16:44:14 +0000

    User commit

--- src/main/java/com/example/demo/entity/PenaltyCalculationEntity.java
+++ src/main/java/com/example/demo/entity/PenaltyCalculationEntity.java
@@ -1,6 +1,16 @@
 package com.example.demo.entity;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+@Entity
+@Data
+@AllArgsConstructor
+@NoAr
 public class PenaltyCalculationEntity{
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private Integer daysDelayed;
     private Double calculatedPenalty;
