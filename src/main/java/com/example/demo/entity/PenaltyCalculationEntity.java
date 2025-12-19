96e148bcc1b77f08c318e996d66b968acfc1ed96
bala-amypo <bala@amypo.in>
2025-12-19 16:41:14 +0000

    User commit

--- src/main/java/com/example/demo/entity/PenaltyCalculationEntity.java
+++ src/main/java/com/example/demo/entity/PenaltyCalculationEntity.java
@@ -3,4 +3,6 @@ package com.example.demo.entity;
 public class PenaltyCalculationEntity{
     private Long id;
     private Integer daysDelayed;
+    private Double calculatedPenalty;
+    private LocalDateTime calculatedAt;
 }
