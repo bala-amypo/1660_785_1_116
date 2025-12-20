8fb7a4af50d0bf623f9d741292e4b4ec936b93f3
bala-amypo <bala@amypo.in>
2025-12-20 05:32:18 +0000

    User commit

--- src/main/java/com/example/demo/repository/VendorTierEntity.java
+++ src/main/java/com/example/demo/repository/VendorTierEntity.java
@@ -4,60 +4,13 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
-import jakarta.persistence.Column;
-import java.time.LocalDateTime;
 
 @Entity
 
-public class UserEntity{
+public class VendorTierEntity{
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-    @Column(unique=true)
-    private String email;
-    private String password;
-    private String roles;
-    private LocalDateTime createdAt;
-
-     public Long getId() {
-        return id;
-    }
-    public void setId(Long id) {
-        this.id = id;
-    }
-    public String getEmail() {
-        return email;
-    }
-    public void setEmail(String email) {
-        this.email = email;
-    }
-    public String getPassword() {
-        return password;
-    }
-    public void setPassword(String password) {
-        this.password = password;
-    }
-    public String getRoles() {
-        return roles;
-    }
-    public void setRoles(String roles) {
-        this.roles = roles;
-    }
-    public LocalDateTime getCreatedAt() {
-        return createdAt;
-    }
-    public void setCreatedAt(LocalDateTime createdAt) {
-        this.createdAt = createdAt;
-    }
-    
-    public UserEntity(Long id, String email, String password, String roles, LocalDateTime createdAt) {
-        this.id = id;
-        this.email = email;
-        this.password = password;
-        this.roles = roles;
-        this.createdAt = createdAt;
-    }
-    public UserEntity() {
-    }
+    private String tierName;
     
 }
