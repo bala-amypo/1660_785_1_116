cf182bee021cfcbb6fd4755fbfec872cc4f2287b
bala-amypo <bala@amypo.in>
2025-12-20 07:01:42 +0000

    User commit

--- src/main/java/com/example/demo/service/Impl/ContractServiceImpl.java
+++ src/main/java/com/example/demo/service/Impl/ContractServiceImpl.java
@@ -4,21 +4,16 @@ import com.example.demo.entity.Contract;
 import com.example.demo.entity.DeliveryRecordEntity;
 import com.example.demo.exception.ResourceNotFoundException;
 import com.example.demo.repository.ContractRepository;
-import com.example.demo.repository.DeliveryRecordRepository;
 import com.example.demo.service.ContractService;
 
 import java.time.LocalDateTime;
-import java.util.List;
 
 public class ContractServiceImpl implements ContractService {
 
     private ContractRepository contractRepo;
-    private DeliveryRecordRepository deliveryRepo;
 
-    public ContractServiceImpl(ContractRepository contractRepo,
-                               DeliveryRecordRepository deliveryRepo) {
+    public ContractServiceImpl(ContractRepository contractRepo) {
         this.contractRepo = contractRepo;
-        this.deliveryRepo = deliveryRepo;
     }
 
     @Override
