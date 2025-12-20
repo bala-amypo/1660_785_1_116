fb770ec86d26369b54053d868d5ba41cd108b36f
bala-amypo <bala@amypo.in>
2025-12-20 07:52:42 +0000

    User commit

--- src/main/java/com/example/demo/service/ContractService.java
+++ src/main/java/com/example/demo/service/ContractService.java
@@ -1,13 +1,13 @@
 package com.example.demo.service;
 
-import com.example.demo.entity.Contract;
+import com.example.demo.entity.ContractEntity;
 import java.util.List;
 
 public interface ContractService {
 
-    Contract createContract(Contract contract);
+    Contract createContract(ContractEntity contract);
 
-    Contract updateContract(Long id, Contract contract);
+    Contract updateContract(Long id, ContractEntity contract);
 
     Contract getContractById(Long id);
 
