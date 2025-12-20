b12ed731c516ac8b225113a895f7c45b5f17cfc7
bala-amypo <bala@amypo.in>
2025-12-20 11:19:07 +0000

    User commit

--- src/main/java/com/example/demo/service/Impl/DeliveryRecordServiceImpl.java
+++ src/main/java/com/example/demo/service/Impl/DeliveryRecordServiceImpl.java
@@ -1,6 +1,6 @@
 package com.example.demo.service.impl;
 
-import org.springframework.stereotype.Service
+import org.springframework.stereotype.Service;
 import com.example.demo.entity.DeliveryRecordEntity;
 import com.example.demo.repository.DeliveryRecordRepository;
 import com.example.demo.repository.ContractRepository;
