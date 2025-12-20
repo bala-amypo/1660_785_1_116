dc86aae03a0a7d27c3df0b884ca28a0fb5988f07
bala-amypo <bala@amypo.in>
2025-12-20 10:05:44 +0000

    User commit

--- src/main/java/com/example/demo/entity/DeliveryRecordEntity.java
+++ src/main/java/com/example/demo/entity/DeliveryRecordEntity.java
@@ -1,67 +1,65 @@
 package com.example.demo.entity;
 
-import java.util.Date;
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
-import jakarta.validation.constraints.Present;
-import jakarta.validation.constraints.Past;
 import jakarta.persistence.ManyToOne;
+
+import java.time.LocalDate;
 import java.time.LocalDateTime;
 
 @Entity
-public class DeliveryRecordEntity{
-     @Id
-    @GeneratedValue(strategy=GenerationType.IDENTITY)
+public class DeliveryRecordEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
     @ManyToOne
     private ContractEntity contract;
-    @PastOrPresent
-    private Date deliveryDate;
+
+    private LocalDate deliveryDate;
     private String notes;
     private LocalDateTime createdAt;
 
-    public void setId(Long id){
-        this.id=id;
+    public DeliveryRecordEntity() {
     }
-    public Long getId(){
+
+    public Long getId() {
         return id;
     }
-    public void setContract(ContractEntity contract){
-        this.contract=contract;
-    }
-    public ContractEntity getContract(){
+
+    public ContractEntity getContract() {
         return contract;
     }
-    public void setDeliveryDate(Date deliveryDate){
-        this.deliveryDate=deliveryDate;
+
+    public void setContract(ContractEntity contract) {
+        this.contract = contract;
     }
-    public  Date getDeliveryDate(){
+
+    public LocalDate getDeliveryDate() {
         return deliveryDate;
     }
-    public void setNotes(String notes){
-        this.notes=notes;
+
+    public void setDeliveryDate(LocalDate deliveryDate) {
+        this.deliveryDate = deliveryDate;
     }
-    public String getNotes(){
+
+    public String getNotes() {
         return notes;
     }
-    public void setCreatedAt(LocalDateTime createdAt){
-        this.createdAt=createdAt;
+
+    public void setNotes(String notes) {
+        this.notes = notes;
     }
-    public LocalDateTime getCreatedAt(){
+
+    public LocalDateTime getCreatedAt() {
         return createdAt;
     }
 
-    public DeliveryRecordEntity(Long id, ContractEntity contract, Date deliveryDate,
-        String notes, LocalDateTime createdAt){
-
-        this.id=id;
-        this.contract=contract;
-        this.deliveryDate=deliveryDate;
-        this.notes=notes;
-        this.createdAt=createdAt;
+    public void setCreatedAt(LocalDateTime createdAt) {
+        this.createdAt = createdAt;
     }
-    public  DeliveryRecordEntity(){
-    }
+    
 }
