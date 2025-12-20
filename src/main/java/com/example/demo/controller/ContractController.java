527347ecc8097b5c81ae9b1b16b32c2c65e35353
bala-amypo <bala@amypo.in>
2025-12-20 07:04:42 +0000

    User commit

--- src/main/java/com/example/demo/controller/ContractController.java
+++ src/main/java/com/example/demo/controller/ContractController.java
@@ -1,8 +1,15 @@
 package com.example.demo.controller;
 
+import com.example.demo.entity.Contract;
+import com.example.demo.service.ContractService;
+
+import java.util.List;
+
 import org.springframework.web.bind.annotation.RestController;
-@RestController
-public class ContractController{
-    
-    
-}
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
+
