582168c4d33c174518adf68635211fd4c9b57c0e
bala-amypo <bala@amypo.in>
2025-12-21 12:32:54 +0000

    User commit

--- src/main/java/com/example/demo/service/PenaltyCalculationService.java
+++ src/main/java/com/example/demo/service/PenaltyCalculationService.java
@@ -6,9 +6,8 @@ import java.util.List;
 public interface PenaltyCalculationService {
 
     // PenaltyCalculation calculatePenalty(Long contractId);
-    PenaltyCalculation save(PenaltyCalculation p)
+    PenaltyCalculation save(PenaltyCalculation p);
     PenaltyCalculation getCalculationById(Long id);
 
     List<PenaltyCalculation> getCalculationsForContract(Long contractId);
 }
-PenaltyCalculation save(PenaltyCalculation p)
