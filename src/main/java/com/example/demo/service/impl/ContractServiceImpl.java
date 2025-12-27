5461c2034831d4c37927d0e1fad39761bd7aecf2
bala-amypo <bala@amypo.in>
2025-12-27 18:15:41 +0000

    User commit

--- src/main/java/com/example/demo/service/impl/ContractServiceImpl.java
+++ src/main/java/com/example/demo/service/impl/ContractServiceImpl.java
@@ -16,13 +16,6 @@ public class ContractServiceImpl implements ContractService {
     ContractRepository contractRepository;
     DeliveryRecordRepository deliveryRecordRepository;
 
-    public ContractServiceImpl(
-            ContractRepository contractRepository,
-            DeliveryRecordRepository deliveryRecordRepository) {
-
-        this.contractRepository = contractRepository;
-        this.deliveryRecordRepository = deliveryRecordRepository;
-    }
     
     @Override
     public Contract createContract(Contract c) {
