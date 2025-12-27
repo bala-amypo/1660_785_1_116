126a828cd93a2356ddce582cd169ed382ff2d20d
bala-amypo <bala@amypo.in>
2025-12-27 17:30:12 +0000

    User commit

--- src/main/java/com/example/demo/entity/DeliveryRecord.java
+++ src/main/java/com/example/demo/entity/DeliveryRecord.java
@@ -34,7 +34,7 @@ import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
-import javax.persistence.*;
+import jakarta.persistence.*;
 import java.time.LocalDate;
 import java.time.LocalDateTime;
 
