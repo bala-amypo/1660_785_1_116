2d720d7ba42287eacdf78558db27b4c92c3680c4
bala-amypo <bala@amypo.in>
2025-12-27 17:44:31 +0000

    User commit

--- src/main/java/com/example/demo/repository/VendorTierRepository.java
+++ src/main/java/com/example/demo/repository/VendorTierRepository.java
@@ -1,8 +1,8 @@
-package com.example.demo.repository;
+// package com.example.demo.repository;
 
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
+// import org.springframework.data.jpa.repository.JpaRepository;
+// import org.springframework.stereotype.Repository;
 
-@Repository
-public interface VendorTierRepository extends JpaRepository<Object, Long> {
-}
+// @Repository
+// public interface VendorTierRepository extends JpaRepository<Object, Long> {
+// }
