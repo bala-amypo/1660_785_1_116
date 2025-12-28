ca393bbb5f1a6be945803ec8228d2fa48dcad4b6
bala-amypo <bala@amypo.in>
2025-12-28 07:32:41 +0000

    User commit

--- src/main/java/com/example/demo/config/OpenApiConfig.java
+++ src/main/java/com/example/demo/config/OpenApiConfig.java
@@ -43,3 +43,35 @@
 //     }
 // }
 
+
+
+package com.example.demo.config; 
+ 
+import org.springframework.context.annotation.Bean; 
+import org.springframework.context.annotation.Configuration; 
+import io.swagger.v3.oas.models.Components; 
+import io.swagger.v3.oas.models.OpenAPI; 
+import io.swagger.v3.oas.models.servers.Server; 
+import io.swagger.v3.oas.models.security.SecurityRequirement; 
+import io.swagger.v3.oas.models.security.SecurityScheme; 
+import java.util.List; 
+ 
+@Configuration 
+public class OpenApiConfig { 
+ 
+    @Bean 
+    public OpenAPI customOpenAPI() { 
+        // Security scheme for JWT 
+        SecurityScheme bearerAuth = new SecurityScheme() 
+                .type(SecurityScheme.Type.HTTP) 
+                .scheme("bearer") 
+                .bearerFormat("JWT"); 
+ 
+        return new OpenAPI() 
+                .components(new Components().addSecuritySchemes("bearerAuth", 
+            bearerAuth)) 
+                .addSecurityItem(new SecurityRequirement().addList("bearerAuth")) 
+                .servers(List.of(new Server().url("https://9185.pro604cr.amypo.ai/"))); // your 
+        server URL 
+} 
+}
