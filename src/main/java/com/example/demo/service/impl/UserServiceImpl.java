4be1d713207fba4f3696ef36b5f469604129f912
bala-amypo <bala@amypo.in>
2025-12-28 09:40:17 +0000

    User commit

--- src/main/java/com/example/demo/service/impl/UserServiceImpl.java
+++ src/main/java/com/example/demo/service/impl/UserServiceImpl.java
@@ -48,55 +48,6 @@
 // }
 
 
-// package com.example.demo.service.impl;
-
-// import com.example.demo.entity.User;
-// import com.example.demo.exception.BadRequestException;
-// import com.example.demo.repository.UserRepository;
-// import com.example.demo.service.UserService;
-// import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-// import org.springframework.stereotype.Service;
-
-// import java.util.Set;
-
-// @Service
-// public class UserServiceImpl implements UserService {
-
-//     UserRepository userRepository;
-//     BCryptPasswordEncoder encoder = new BCryptPasswordEncoder();
-
-//     public UserServiceImpl() {
-//     }
-
-//     public UserServiceImpl(UserRepository userRepository) {
-//         this.userRepository = userRepository;
-//     }
-
-//     @Override
-//     public User registerUser(String email, String password) {
-
-//         if (userRepository.existsByEmail(email)) {
-//             throw new BadRequestException("Email already exists");
-//         }
-
-//         User user = User.builder()
-//                 .email(email)
-//                 .password(encoder.encode(password))
-//                 .roles(Set.of("ROLE_USER"))
-//                 .build();
-
-//         return userRepository.save(user);
-//     }
-
-//     @Override
-//     public User getUserByEmail(String email) {
-//         return userRepository.findByEmail(email)
-//                 .orElseThrow(() ->
-//                         new BadRequestException("User not found"));
-//     }
-// }
-
-
 package com.example.demo.service.impl;
 
 import com.example.demo.entity.User;
@@ -111,14 +62,14 @@ import java.util.Set;
 @Service
 public class UserServiceImpl implements UserService {
 
-    private final UserRepository userRepository;
-    private final BCryptPasswordEncoder encoder;
+    UserRepository userRepository;
+    BCryptPasswordEncoder encoder = new BCryptPasswordEncoder();
 
-    // ✅ ONLY constructor
-    public UserServiceImpl(UserRepository userRepository,
-                           BCryptPasswordEncoder encoder) {
+    public UserServiceImpl() {
+    }
+
+    public UserServiceImpl(UserRepository userRepository) {
         this.userRepository = userRepository;
-        this.encoder = encoder;
     }
 
     @Override
@@ -144,3 +95,5 @@ public class UserServiceImpl implements UserService {
                         new BadRequestException("User not found"));
     }
 }
+
+
